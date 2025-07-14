7c767808839f12f8e86c1a39e67cf0ed3cc64d0d
Admin Admin <admin@eduaigen.org>
2025-07-14 00:53:04 +0000

    ADD GLOBAL HISTORY 1 CURRICULLUM {   "curriculumTitle": "Global History

--- src/lib/curriculum-data.ts
+++ src/lib/curriculum-data.ts
@@ -370,6 +370,39 @@ export const curriculumData: {
     },
     History: {
       units: {
+        "Age of Revolutions": {
+          topics: {
+            "Enlightenment": { "lessons": ["Enlightenment"] },
+            "French Revolution": { "lessons": ["French Revolution"] },
+            "Industrial Revolution": { "lessons": ["Industrial Revolution"] }
+          }
+        },
+        "Imperialism and Nationalism": {
+          topics: {
+            "Africa and Asia": { "lessons": ["Africa and Asia"] },
+            "Latin America": { "lessons": ["Latin America"] },
+            "Resistance Movements": { "lessons": ["Resistance Movements"] }
+          }
+        },
+        "20th Century Conflicts": {
+          topics: {
+            "World Wars": { "lessons": ["World Wars"] },
+            "Cold War": { "lessons": ["Cold War"] },
+            "Genocide": { "lessons": ["Genocide"] }
+          }
+        },
+        "Contemporary Issues": {
+          topics: {
+            "Globalization": { "lessons": ["Globalization"] },
+            "Terrorism": { "lessons": ["Terrorism"] },
+            "Environmental Challenges": { "lessons": ["Environmental Challenges"] }
+          }
+        },
+        "Closing Performance Task": {
+          topics: {
+            "Global History Regents Review": { "lessons": ["Global History Regents Review"] }
+          }
+        },
         "Constitutional Foundations": {
           topics: {
             "Colonial America": { "lessons": ["Colonial America"] },
